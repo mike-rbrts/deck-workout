918467cae6d12ee546f0a0e41b1175ba7ca75d82
Michael Roberts <roberts.b.michael@gmail.com>
2023-08-28 17:11:31 -0500

    MR | Implement initial layout

---
+++ DeckWorkout/DeckSelectionVC.swift
@@ -0,0 +1,29 @@
+//
+//  DeckSelectionVC.swift
+//  DeckWorkout
+//
+//  Created by Mike Roberts on 8/28/23.
+//
+
+import UIKit
+
+class DeckSelectionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
